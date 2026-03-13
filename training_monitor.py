7f0a847d52fcc80ea8da74ec45911100b3256e12
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-03-13 23:19:26 +0530

    Add gradient norm tracking and feature extraction
    add function for getting features

--- training_monitor.py
+++ training_monitor.py
@@ -16,3 +16,21 @@ class TrainingMonitor:
             if p.grad is not None:
                 param_norm = p.grad.data.norm(2)
                 total_norm += param_norm.item() ** 2
+
+        total_norm = total_norm ** 0.5
+
+        self.gradient_norms.append(total_norm)
+
+    def get_features(self):
+
+        loss_slope = self.loss_hisotry[-1] - self.loss_history[0]
+
+        avg_grad = sum(self.gradient_norms) / len(self.gradient_norms)
+
+        grad_var = torch.tensor(self.gradient_norms).var().items()
+
+        return {
+            "loss_slope": loss_slope,
+            "avg_grad_norm": avg_grad,
+            "grad_variance": grad_var,
+        }
