cba331caa64b546a3155b51cd4eef0e443cf75f7
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-03-14 23:17:39 +0530

    Update failure_predictor.py

--- failure_predictor.py
+++ failure_predictor.py
@@ -1,23 +1,27 @@
-import torch
-def sample_loss_landscape(model, loss_fn, data, target, epsilon=1e-3):
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
 
-    original_params = []
+class FailurePrediction:
 
-    for p in model.parameters(): # model.parameters() returns an iterator over all the parameters of the model
-        original_params.append(p.data.clone()) # clone the data of the parameter and store it in original_params
+    def __init__(self):
 
-        noise = []
+        self.model = RandomForestClassifier()
 
-        for p in model.parameters():
-            noise.append(torch.rand_like(p) * epsilon) # generate random noise of the same shape as the parameter and scale it by epsilon
+        self.X = []
+        self.y = []
 
-        for p, n in zip(model.parameters(), noise):
-            p.data += n
-        output = model(data)
+    def add_example(self, features, label):
 
-        perturbed_loss = loss_fn(output, target).item()
+        x = [
+            features['loss_slope'],
+            features['avg_grad_norm'],
+            features['grad_variance'],
+        ]
 
-        for p, orig in zip(model.parameters(), original_params):
-            p.data = orig
-        return perturbed_loss
-    
+        self.X.append(x)
+
+        self.y.append(label)
+
+    def train(self):
+
+        self.model.fit(np.array(self.X), np.array(self.y))
