5c7adabafd4f3a219991c1924b755226529b5a80
Sanjay Kumar Sakamuri Kamalakar <sksanjaykumar010307@gmail.com>
2026-03-14 23:24:57 +0530

    Training loop with monitoring.

--- train.py
+++ train.py
@@ -1,1 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
 
+from model import SimpleCNN
+from training_monitor import TrainingMonitor
+
+def train_model(epochs=5):
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    transform = transforms.ToTensor()
+
+    dataset = datasets.MNIST("./data", train=True, download=True, transform=transform)
+
+    loader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
+
+    model = SimpleCNN().to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    loss_fn = nn.CrossEntropyLoss()
+
+    monitor = TrainingMonitor()
+
+    
